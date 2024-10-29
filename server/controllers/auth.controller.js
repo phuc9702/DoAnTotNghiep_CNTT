c523425b75903c6ef19e964477361ed77a36a7d5
Pham Ngoc Phuc <phamngocphuc9702@gmail.com>
2024-10-29 13:09:38 +0700

    error handle 2

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const db = require('../models')
 const brcypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const hasPassword = password => brcypt.hashSync(password, brcypt.genSaltSync(10));
 module.exports={
@@ -8,15 +9,22 @@ module.exports={
     loginWithGoogle: asyncHandler(async (req, res) => {
         const {email, fullname, avatar, password} = req.body
 
+        let uid
+
         const aleadyUser = await db.User.findOne({where: {email}})
         
         if(!aleadyUser) {
-            const response =  await db.User.create({email, fullname, avatar,password: hasPassword(password)})
-            if(!response) throw new [errror("lỗi tạo mới ")]
+            const newUser =  await db.User.create({email, fullname, avatar,password: hasPassword(password)})
+            if(!newUser) throw new Error("lỗi tạo mới. ")
+                uid = newUser.id
         }
+        uid = aleadyUser.id
+
+        const token = jwt.sign({uid}, process.env.SECRET_JWT_KEY, {expiresIn:"7d"})
+
         return res.json({
-            success:true,
-            aleadyUser,
+            success:!!token,
+            accessToken: token,
         
         })
     }),
